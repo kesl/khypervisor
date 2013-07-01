b2652d685934e1e613dd61031fc4149f97b946e5
Simon Kim <spamhere@kaoma.net>
2013-07-01 19:48:09 +0900

    Reverted Unecessary commits

--- monitor/uart_print.h
+++ monitor/uart_print.h
@@ -2,12 +2,9 @@
 #define __UART_PRINT_H__
 #include "arch_types.h"
 
-#define HVMM_TRACE_ENTER()	{uart_print( __FUNCTION__ );  uart_print("() - enter\n\r");}
-#define HVMM_TRACE_EXIT()	{uart_print( __FUNCTION__ );  uart_print("() - exit\n\r");}
-
-void uart_putc( const char c );
-void uart_print(const char *str);
-void uart_print_hex32( uint32_t v );
+void uart_putc( char c );
+void uart_print(char *str);
+void uart_print_hex32( unsigned int v );
 void uart_print_hex64( uint64_t v );
 
 #endif
