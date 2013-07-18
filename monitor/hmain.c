79aec5e049ceb215803468451e6d0f09ed024519
inkyu <in10004kyu@gmail.com>
2013-07-18 19:49:00 +0900

    1. Generic Timer signals a PPI to GIC that calls ISR for guest context switch. 2. Time unit is microseconds. 3. Modify generic_tiomer.c, generic_timer.h, scheduler.c, scheduler.h, timer.c, timer.h, Makefile 4. Add asm-arm_inline.h, generic_timer_regs.h

--- monitor/hmain.c
+++ monitor/hmain.c
@@ -7,8 +7,7 @@
 #include "gic.h"
 #include "interrupt.h"
 #include "context.h"
-
-
+#include "scheduler.h"
 
 void hyp_abort_infinite(void)
 {
@@ -108,7 +107,9 @@ void hyp_main(void)
 	context_init_guests();
 
 	/* Interrupt test */
-	hvmm_interrupt_test();
+	//hvmm_interrupt_test();
+	scheduler_test_scheduling();
+	
 
 	/* Switch to the first guest */
 	context_switch_guest();
--- monitor/scheduler.h
+++ monitor/scheduler.h
@@ -1,20 +1,13 @@
 #ifndef __SCHEDULER_H__
 #define __SCHEDULER_H__
 
-#include "hvmm_types.h"
+#include "armv7_p15.h"
+#include "context.h"
+#include "uart_print.h"
 #include "timer.h"
 
-/*
-*		
-*/
-hvmm_status_t scheduler_init(void);
-/*
-*	call back fucntion. Called when timer interrupt occur
-*
-*	e.g)
-*	timer_add_tick_callback(30, &scheduler_next_event);
-*	_gic_handlers[irq](irq, regs);
-*/
 hvmm_status_t scheduler_next_event(int irq, void *pdata);
 
+/* Test Code */
+void scheduler_test_scheduling(void);
 #endif
