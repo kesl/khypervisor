cfb43aa1f49d86610b316b7352c426fc7907ecce
Simon Kim <spamhere@kaoma.net>
2013-11-15 12:30:03 +0900

    Upon hvc-dabort, hypervisor stops the system and dumps registers(+banked) of the guest that caused the dabort for better debugging. vdev_sample test code in bmguest has been disabled

--- monitor/trap_dabort.h
+++ monitor/trap_dabort.h
@@ -1,6 +1,7 @@
 #ifndef __TRAP_DABORT_H__
 #define __TRAP_DABORT_H__
+#include <hvmm_types.h>
 
-void trap_hvc_dabort(unsigned int iss, struct arch_regs *regs);
+hvmm_status_t trap_hvc_dabort(unsigned int iss, struct arch_regs *regs);
 
 #endif
