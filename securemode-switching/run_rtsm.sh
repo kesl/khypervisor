c62caa3c9f33616765bc5e0a68fd3872fc065e6e
joohyun kyong <joohyun0115@gmail.com>
2013-12-30 10:54:57 +0900

    layout design : 1. refactor directory layout
    1. monitor -> hypervisor
    2. platform files -> platform-device/<platform_name>
    3. delete unnecessary files
    4. change submodule url

--- securemode-switching/run_rtsm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=/usr/local/RTSM_A15-A7x14_VE/models/Linux64_GCC-4.1/ RTSM_VE_Cortex-A15x1-A7x1 --cadi-server -a coretile.cluster0.cpu0=secmon.axf
-#modeldebugger /home/simon/ARM/FastModelsPortfolio_8.0/examples/RTSM_VE/Build_Cortex-A15x1/Linux64-Release-GCC-4.4/cadi_system_Linux64-Release-GCC-4.4.so /home/simon/Virtualization/boot-wrapper/linux-system-semi.axf
