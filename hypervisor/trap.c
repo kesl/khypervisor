c7deef6ae698e802b2858cce585e0ffd888f4ea4
Wonseok <magicyaba@gmail.com>
2014-02-19 21:43:59 -0800

    modified trap handler

--- hypervisor/trap.c
+++ hypervisor/trap.c
@@ -9,63 +9,61 @@
 #include <log/print.h>
 
 /* By holding the address to the saved regs struct, 
-   context or other modules can access to this structure
-   through trap_saved_regs() call when it's needed. 
-   For example, copying register values for context switching can be 
-   performed this way.
+ context or other modules can access to this structure
+ through trap_saved_regs() call when it's needed.
+ For example, copying register values for context switching can be
+ performed this way.
  */
 
 static struct arch_regs *_trap_hyp_saved_regs = 0;
 
-hvmm_status_t _hyp_trap_dabort( struct arch_regs *regs )
-{
+hvmm_status_t _hyp_trap_dabort(struct arch_regs *regs) {
 
-    _trap_hyp_saved_regs = regs;
+	_trap_hyp_saved_regs = regs;
 
-    context_dump_regs( regs );
-    hyp_abort_infinite();
+	context_dump_regs(regs);
+	hyp_abort_infinite()
+	;
 
-    return HVMM_STATUS_UNKNOWN_ERROR;
+	return HVMM_STATUS_UNKNOWN_ERROR;
 }
 
-hvmm_status_t _hyp_trap_irq( struct arch_regs *regs )
-{
+hvmm_status_t _hyp_trap_irq(struct arch_regs *regs) {
 
-    _trap_hyp_saved_regs = regs;
+	_trap_hyp_saved_regs = regs;
 
-    gic_interrupt(0, regs);
+	gic_interrupt(0, regs);
 
-    context_perform_switch();
-    return HVMM_STATUS_SUCCESS;
+	context_perform_switch();
+	return HVMM_STATUS_SUCCESS;
 }
 
-hvmm_status_t _hyp_trap_unhandled( struct arch_regs *regs )
-{
-    _trap_hyp_saved_regs = regs;
+hvmm_status_t _hyp_trap_unhandled(struct arch_regs *regs) {
+	_trap_hyp_saved_regs = regs;
 
-    context_dump_regs( regs );
-    hyp_abort_infinite();
+	context_dump_regs(regs);
+	hyp_abort_infinite()
+	;
 
-    return HVMM_STATUS_UNKNOWN_ERROR;
+	return HVMM_STATUS_UNKNOWN_ERROR;
 }
 
-static void _trap_dump_bregs(void)
-{
-    uint32_t spsr, lr, sp;
-    printh( " - banked regs\n" );
-    asm volatile (" mrs     %0, sp_usr\n\t" :"=r" (sp)::"memory", "cc");
-    asm volatile (" mrs     %0, lr_usr\n\t" :"=r" (lr)::"memory", "cc");
-    printh( " - usr: sp:%x lr:%x\n", sp, lr );
+static void _trap_dump_bregs(void) {
+	uint32_t spsr, lr, sp;
+	printh(" - banked regs\n");
+	asm volatile (" mrs     %0, sp_usr\n\t" :"=r" (sp)::"memory", "cc");
+	asm volatile (" mrs     %0, lr_usr\n\t" :"=r" (lr)::"memory", "cc");
+	printh(" - usr: sp:%x lr:%x\n", sp, lr);
 
-    asm volatile (" mrs     %0, spsr_svc\n\t" :"=r" (spsr)::"memory", "cc");
-    asm volatile (" mrs     %0, sp_svc\n\t" :"=r" (sp)::"memory", "cc");
-    asm volatile (" mrs     %0, lr_svc\n\t" :"=r" (lr)::"memory", "cc");
-    printh( " - svc: spsr:%x sp:%x lr:%x\n", spsr, sp, lr );
+	asm volatile (" mrs     %0, spsr_svc\n\t" :"=r" (spsr)::"memory", "cc");
+	asm volatile (" mrs     %0, sp_svc\n\t" :"=r" (sp)::"memory", "cc");
+	asm volatile (" mrs     %0, lr_svc\n\t" :"=r" (lr)::"memory", "cc");
+	printh(" - svc: spsr:%x sp:%x lr:%x\n", spsr, sp, lr);
 
-    asm volatile (" mrs     %0, spsr_irq\n\t" :"=r" (spsr)::"memory", "cc");
-    asm volatile (" mrs     %0, sp_irq\n\t" :"=r" (sp)::"memory", "cc");
-    asm volatile (" mrs     %0, lr_irq\n\t" :"=r" (lr)::"memory", "cc");
-    printh( " - irq: spsr:%x sp:%x lr:%x\n", spsr, sp, lr );
+	asm volatile (" mrs     %0, spsr_irq\n\t" :"=r" (spsr)::"memory", "cc");
+	asm volatile (" mrs     %0, sp_irq\n\t" :"=r" (sp)::"memory", "cc");
+	asm volatile (" mrs     %0, lr_irq\n\t" :"=r" (lr)::"memory", "cc");
+	printh(" - irq: spsr:%x sp:%x lr:%x\n", spsr, sp, lr);
 }
 
 /*
@@ -76,79 +74,103 @@ static void _trap_dump_bregs(void)
  * IL[25] is an instruction length for the trapped insruction that is 16 bit or 32 bit
  * ISS[24:0] is an instruction-specific syndrome for the instruction information included. It depends on EC field.
  * END OF HSR DESCRIPTION FROM ARM DDI0406_C ARCHITECTURE MANUAL
- */ 
-hyp_hvc_result_t _hyp_hvc_service(struct arch_regs *regs)
-{
-		unsigned int hsr = read_hsr();
-		unsigned int iss = hsr & 0x1FFFFFF;
-		unsigned int ec = (hsr >> 26);
+ */
 
-		_trap_hyp_saved_regs = regs;
+// HSR Exception Class.
+#define TRAP_EC_ZERO_UNKNOWN		0x00
+#define TRAP_EC_ZERO_WFI_WFE		0x01
+#define TRAP_EC_ZERO_MCR_MRC_CP15	0x03
+#define TRAP_EC_ZERO_MCRR_MRRC_CP15	0x04
+#define TRAP_EC_ZERO_MCR_MRC_CP14	0x05
+#define TRAP_EC_ZERO_LDC_STC_CP14	0x06
+#define TRAP_EC_ZERO_HCRTR_CP0_CP13 0x07
+#define TRAP_EC_ZERO_MRC_VMRS_CP10  0x08
+#define TRAP_EC_ZERO_BXJ    0x0A
+#define TRAP_EC_ZERO_MRRC_CP14  0x0C
+#define TRAP_EC_NON_ZERO_SVC    0x11
+#define TRAP_EC_NON_ZERO_HVC    0x12
+#define TRAP_EC_NON_ZERO_SMC    0x13
+#define TRAP_EC_NON_ZERO_PREFETCH_ABORT_FROM_OTHER_MODE 0x20
+#define TRAP_EC_NON_ZERO_PREFETCH_ABORT_FROM_HYP_MODE   0x21
+#define TRAP_EC_NON_ZERO_DATA_ABORT_FROM_OTHER_MODE 0x24
+#define TRAP_EC_NON_ZERO_DATA_ABORT_FROM_HYP_MODE   0x25
 
-		printh("[hvc] _hyp_hvc_service: enter\n\r");
+// HSR Bit Extraction.
+#define HSR_EC_ZERO  0xC0000000
+#define EXTRACT_EC_ZERO 30
+#define HSR_EC_BIT  0xFC000000
+#define EXTRACT_EC 26
+#define HSR_IL_BIT  0x2000000
+#define EXTRACT_IL 25
+#define HSR_ISS_BIT  0x1FFFFFF
 
-    if ( ec == 0x12 && ((iss & 0xFFFF) == 0xFFFF) ) {
-        /* Internal request to stay in hyp mode */
-        printh("[hvc] enter hyp\n\r");
-        context_dump_regs( regs );
-        return HYP_RESULT_STAY;
-    }
+hyp_hvc_result_t _hyp_hvc_service(struct arch_regs *regs) {
+	unsigned int hsr = read_hsr();
+	unsigned int iss = hsr & 0x1FFFFFF;
+	unsigned int ec = (hsr >> 26);
 
-    if ( ec == 0x24) {
-        /* Handle data abort at the priority */
-        printh("[hyp] data abort handler: hsr.iss=%x\n", iss);
-        _trap_dump_bregs();
+	_trap_hyp_saved_regs = regs;
 
-        if ( trap_hvc_dabort(iss, regs) != HVMM_STATUS_SUCCESS ) {
-            printh( "[hyp] === Unhandled dabort ===\n" );
-            printh( "[hyp] current guest vmid:%d\n", context_current_vmid() );
-            context_dump_regs( regs );
+	printh("[hvc] _hyp_hvc_service: enter\n\r");
 
-            _trap_dump_bregs();
+	switch (ec) {
+	case TRAP_EC_NON_ZERO_HVC: {
+		if ((iss & 0xFFFF) == 0xFFFF) {
+			/* Internal request to stay in hyp mode */
+			printh("[hvc] enter hyp\n\r");
+			context_dump_regs(regs);
+			return HYP_RESULT_STAY;
+		}
+		/* Handle the other cases */
+		switch (iss)
+		case 0xFFFE:
+			/* hyp_ping */
+			printh("[hyp] _hyp_hvc_service:ping\n\r");
+			context_dump_regs(regs);
+			break;
+		case 0xFFFD:
+			/* hsvc_yield() */
+			printh("[hyp] _hyp_hvc_service:yield\n\r");
+			context_dump_regs(regs);
+			context_switchto(sched_policy_determ_next());
+			break;
+	} break; //TRAP_EC_NON_ZERO_HVC.
+	case TRAP_EC_NON_ZERO_DATA_ABORT_FROM_OTHER_MODE: {
+		/* Handle data abort at the priority */
+		printh("[hyp] data abort handler: hsr.iss=%x\n", iss);
+		_trap_dump_bregs();
 
-            hyp_abort_infinite();
-        }
-        _trap_dump_bregs();
-    } else {
-        /* Handle the other cases */
-        switch( iss ) {
-            case 0xFFFE:
-                /* hyp_ping */
-                printh("[hyp] _hyp_hvc_service:ping\n\r");
-                context_dump_regs( regs );
-                break;
-            case 0xFFFD:
-                {
-                    /* hsvc_yield() */
-                    printh("[hyp] _hyp_hvc_service:yield\n\r");
-                    context_dump_regs( regs );
-                    context_switchto(sched_policy_determ_next());
-                }
-                break;
-            default:
-                if ( ec == 0x20 ) {
-                    // Prefetch Abort routed to Hyp mode
-                    printh( "[hyp]: prefetch abort routed to Hyp mode\n");
-                }
-    
-                printh("[hyp] _hyp_hvc_service:unknown hsr.iss= %x\n", iss);
-                printh("[hyp] hsr.ec= %x\n", ec);
-                printh("[hyp] hsr= %x\n", hsr);
-                context_dump_regs( regs );
-                _trap_dump_bregs();
-                hyp_abort_infinite();
-                break;
-        }
-    }
-    printh("[hyp] _hyp_hvc_service: done\n\r");
+		if (trap_hvc_dabort(iss, regs) != HVMM_STATUS_SUCCESS) {
+			printh("[hyp] === Unhandled dabort ===\n");
+			printh("[hyp] current guest vmid:%d\n", context_current_vmid());
+			context_dump_regs(regs);
+			_trap_dump_bregs();
+			hyp_abort_infinite();
+		}
+		_trap_dump_bregs();
+	}
+		break; //TRAP_EC_NON_ZERO_DATA_ABORT_FROM_OTHER_MODE.
+	case TRAP_EC_NON_ZERO_PREFETCH_ABORT_FROM_OTHER_MODE:
+		printh("[hyp]: prefetch abort routed to Hyp mode\n");
+		break; // TRAP_EC_NON_ZERO_PREFETCH_ABORT_FROM_OTHER_MODE.
+	default:
+		printh("[hyp] _hyp_hvc_service:unknown hsr.iss= %x\n", iss);
+		printh("[hyp] hsr.ec= %x\n", ec);
+		printh("[hyp] hsr= %x\n", hsr);
+		context_dump_regs(regs);
+		_trap_dump_bregs();
+		hyp_abort_infinite()
+		;
+		break;
+	} // End of switch(ec) statement.
 
-    context_perform_switch();
-    return HYP_RESULT_ERET;
+	printh("[hyp] _hyp_hvc_service: done\n\r");
+	context_perform_switch();
+	return HYP_RESULT_ERET;
 }
 
 /* API */
 
-struct arch_regs * trap_saved_regs(void)
-{
-    return _trap_hyp_saved_regs;
+struct arch_regs * trap_saved_regs(void) {
+	return _trap_hyp_saved_regs;
 }
