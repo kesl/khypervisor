99f11fbddf15acfdf168262c58420a1c230fef9a
joohyun kyong <joohyun0115@gmail.com>
2014-02-26 06:50:51 +0900

    test4

--- hypervisor/sched_policy.c
+++ hypervisor/sched_policy.c
@@ -4,13 +4,8 @@
 vmid_t sched_policy_determ_next(void)
 {
     vmid_t next = context_next_vmid(context_current_vmid());
-<<<<<<< HEAD
     if (next == VMID_INVALID) {
         next = context_first_vmid();
     }
-=======
-    if ( next == VMID_INVALID )
-		next = context_first_vmid();
->>>>>>> 48cde8e1e0b7863138c41fdcdde3e065eea42637
     return next;
 }
