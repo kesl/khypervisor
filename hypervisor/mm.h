177024b35420b905ad6d3fc78b7ce4bf878d9b4e
xiane <sangch.go@gmail.com>
2014-03-04 17:06:17 +0900

    Update mm.h

--- hypervisor/mm.h
+++ hypervisor/mm.h
@@ -17,30 +17,30 @@
  * - Fifth, Enable PL2 Stage 1 MMU by configuring HSCTLR
  * - Sixth, Initialize heap Area<br>
  * @param void
- * @return int HVMM_STATUS_SUCCESS
+ * @return HVMM_STATUS_SUCCESS
  */
 int hvmm_mm_init(void);
 /**
  * @brief Malloc, get size of space
  *
  * Get dynamic Memory from Heap Area
- * @param unsigned long size size of space
- * @return void * allocated space
+ * @param size size of space
+ * @return allocated space
  */
 void *hmm_malloc(unsigned long size);
 /**
  * @brief Free Allocated Memory
  *
  * Free memory
- * @param void* addr Target Area's address
+ * @param * addr Target Area's address
  * @return void
  */
 void hmm_free(void *addr);
 /**
  * @brief Unmapping level 3 table entry in virtual address
  *
- * @param unsigned long virt virtual address
- * @param unsigned long npages number of pages
+ * @param virt virtual address
+ * @param npages number of pages
  * @return void
  */
 void hmm_umap(unsigned long virt, unsigned long npages);
@@ -49,9 +49,9 @@ void hmm_umap(unsigned long virt, unsigned long npages);
  *
  * maping physical address to level 3 table entry<br>
  * that be finded by virtual address and flush TLB
- * @param unsigned long phys target physical address
- * @param unsigned long virt target virtal address
- * @param unsigned long npages number of pages
+ * @param phys target physical address
+ * @param virt target virtal address
+ * @param npages number of pages
  * @return void
  */
 void hmm_map(unsigned long phys, unsigned long virt, unsigned long npages);
