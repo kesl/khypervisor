2768a90d20323b52d9c09e1268f1a6ef8ea85ceb
joohyun kyong <joohyun0115@gmail.com>
2014-03-07 05:42:16 +0900

    smp : smp_processor_id move to inline function

--- hypervisor/include/smp.h
+++ hypervisor/include/smp.h
@@ -1,8 +1,10 @@
+#ifndef __SMP_H__
+#define __SMP_H__
 
+#include "arch_types.h"
 #include "armv7_p15.h"
-#include "smp.h"
 
-uint32_t smp_processor_id(void)
+static inline uint32_t smp_processor_id(void)
 {
     /* Cortex-A15,
      * MPIDR[1:0] - CPUID - 0,1, 2, or 3
@@ -11,3 +13,5 @@ uint32_t smp_processor_id(void)
     uint32_t id = read_mpidr() & 0x0FF;
     return id;
 }
+
+#endif
