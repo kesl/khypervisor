c8cfb6f0221e865016497cb862c169b1bbe2c760
joohyun kyong <joohyun0115@gmail.com>
2014-03-07 16:32:25 +0900

    refactor vector and smp function

--- hypervisor/include/smp.h
+++ hypervisor/include/smp.h
@@ -4,14 +4,15 @@
 #include "arch_types.h"
 #include "armv7_p15.h"
 
+#define MPIDR_MASK 0xFFFFFF
+
 static inline uint32_t smp_processor_id(void)
 {
     /* Cortex-A15,
      * MPIDR[1:0] - CPUID - 0,1, 2, or 3
      * MPIDR[7:2] - Reserved, Read as zero
      */
-    uint32_t id = read_mpidr() & 0x0FF;
-    return id;
+    return read_mpidr() & MPIDR_MASK;
 }
 
 #endif
