5e080c6df420e3db5e38b132d1ebcd62275553a3
inkyu <in1004kyu@gmail.com>
2014-03-10 13:14:29 +0900

    CLI interface : Add boot command

---
+++ common/guest/command/command.h
@@ -0,0 +1,6 @@
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
+void load_cmd_help(int argc, char **argv);
+void load_cmd_boot(int argc, char **argv);
+void load_exec(char *line);
+#endif
--- common/log/uart_print.h
+++ common/log/uart_print.h
@@ -2,10 +2,12 @@
 #define __UART_PRINT_H__
 #include "arch_types.h"
 
-
 void uart_putc(const char c);
 void uart_print(const char *str);
 void uart_print_hex32(uint32_t v);
 void uart_print_hex64(uint64_t v);
+char uart_getc(void);
+void uart_gets(char *s, int maxwidth, char endchar);
+void uart_init(void);
 
 #endif
