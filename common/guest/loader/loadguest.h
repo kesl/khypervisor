445301bc12528e6b1de69ab628554d37ee6adc71
inkyu <in1004kyu@gmail.com>
2014-03-10 13:14:29 +0900

    [GuestLoader]: Add autoboot for guestloader
    1. Add CLI (command.*)
    2. Add autoboot (main.c)
    3. Fix guestloader's memory map(Add sp for system/user mode)
    
    CLI is worked on RTSM. Arndale is not supproted.

--- common/guest/loader/loadguest.h
+++ common/guest/loader/loadguest.h
@@ -2,6 +2,27 @@
 #define __LOADGUEST_H_
 #include <guestloader.h>
 #include <arch_types.h>
+#define GUEST_TYPE_BM       0
+#define GUEST_TYPE_LINUX    1
+#define GUEST_TYPE_RTOS     2
+#define START_ADDR_BM       0x80000000
+#define START_ADDR_LINUX    0xA0008000
+#define START_ADDR_RTOS     0x80000000
+#if defined(LINUX_GUEST)
+#define START_ADDR START_ADDR_LINUX
+#define GUEST_TYPE GUEST_TYPE_LINUX
+#endif
+#if defined(RTOS_GUEST)
+#define START_ADDR START_ADDR_BM
+#define GUEST_TYPE GUEST_TYPE_BM
+#endif
+#if defined(BM_GUEST)
+#define START_ADDR START_ADDR_RTOS
+#define GUEST_TYPE GUEST_TYPE_RTOS
+#endif
+extern uint32_t guest_bin_start;
+extern uint32_t guest_bin_end;
+extern uint32_t loader_start;
 /**
 * @brief Copying Guestloader next to guestimage.
 */
@@ -21,5 +42,10 @@ void load_linuxguest(uint32_t start_addr);
 * @param start_addr Start address of bmguest.
 */
 void load_bmguest(uint32_t start_addr);
+/**
+* @brief Load guest.
+* @param type Guest type. There are bmguest and Linux guest.
+*/
+void load_guest(uint32_t type);
 #endif
 
--- platform-device/cortex_a15x2_rtsm/guestos/guestloader/drivers/uart.c
+++ platform-device/cortex_a15x2_rtsm/guestos/guestloader/drivers/uart.c
@@ -1,17 +1,47 @@
-/* UART Base Address determined by Hypervisor's Stage 2 Translation Table */
-#define UART_BASE       ((volatile unsigned int *) 0x1C090000)
-void uart_print(char *str)
+#include "arch_types.h"
+#include <log/uart_print.h>
+#include "pl011.h"
+#define UART_BASE  PL011_BASE
+#define UART_INCLK 24000000
+#define UART_BAUD  115200
+void uart_putc(const char c)
 {
-    char *pUART = (char *) UART_BASE;
+    if (c == '\n')
+        pl011_putc('\r');
+    pl011_putc(c);
+}
+
+void uart_print(const char *str)
+{
+    while (*str) {
+        uart_putc(*str);
+        str++;
+    }
+    volatile char *pUART = (char *) UART_BASE;
     while (*str)
         *pUART = *str++;
 }
-void uart_putc(char c)
+
+#define UART_PRINT_BUF  12
+void uart_print_dec(uint32_t v)
 {
-    volatile char *pUART = (char *) UART_BASE;
-    *pUART = c;
+    char print_buf[UART_PRINT_BUF];
+    char *s;
+    int i;
+    s = print_buf + UART_PRINT_BUF - 1;
+    *s = '\0';
+    if (v == 0UL)
+        *--s = '0';
+    else {
+        for (; v != 0UL;) {
+            *--s = ((v % 10) + '0');
+            v /= 10;
+        }
+    }
+    uart_print(s);
 }
-void uart_print_hex32(unsigned int v)
+
+void uart_print_hex32(uint32_t v)
 {
     unsigned int mask8 = 0xF;
     unsigned int c;
@@ -25,4 +55,51 @@ void uart_print_hex32(unsigned int v)
             c += 'A' - 10;
         uart_putc((char) c);
     }
+}
+
+void uart_print_hex64(uint64_t v)
+{
+    uart_print_hex32(v >> 32);
+    uart_print_hex32((uint32_t)(v & 0xFFFFFFFF));
+}
+
+int uart_tst_fifo(void)
+{
+    if (!pl011_tst_fifo(UART_BASE))
+        return 0;
+    else
+        return 1;
+}
+
+char uart_getc()
+{
+    char ch = pl011_getc(UART_BASE);
+    if (ch == '\r')
+        ch = '\n';
+    uart_putc(ch);
+    return ch;
+}
+
+void uart_gets(char *s, int maxwidth, char endchar)
+{
+    char *retval;
+    char ch;
+    retval = s;
+    ch = uart_getc();
+    while (ch != endchar && maxwidth > 0) {
+        *retval = ch;
+        retval++;
+        maxwidth--;
+        if (maxwidth == 0)
+            break;
+        ch = uart_getc();
+    }
+    *retval = '\0';
+}
+
+void uart_init(void)
+{
+    pl011_init(UART_BASE,
+            UART_BAUD,
+            UART_INCLK);
 }
