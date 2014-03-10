8576d1116754df2cff95099d83a10273e1fc9a98
inkyu <in1004kyu@gmail.com>
2014-03-10 13:14:29 +0900

    [GuestLoader]: Add autoboot for guestloader
    1. Add CLI (command.*)
    2. Add autoboot (main.c)
    3. Fix guestloader's memory map(Add sp for system/user mode)
    
    CLI is worked on RTSM. Arndale is not supproted.

---
+++ common/guest/command/command.h
@@ -0,0 +1,14 @@
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
+/**
+ * @brief Executes command.
+ *
+ * help (h)        - List commands and their usage
+ * boot (h)        - Boot guestos
+ * banner (ba)     - Showing K-hypervisor banner
+ * status (s)      - Showing hypervisor status
+ *
+ * @param line The string of command.
+ */
+void command_exec(char *line);
+#endif
