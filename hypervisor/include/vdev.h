6262134d0c67576052b5056d3a4c84972cda76d0
joohyun kyong <joohyun0115@gmail.com>
2014-03-11 11:32:11 +0900

    virtual device : Removed ARM architecture dependencies in virtual device.

--- hypervisor/include/vdev.h
+++ hypervisor/include/vdev.h
@@ -6,32 +6,133 @@
 
 enum vdev_access_size {
     VDEV_ACCESS_BYTE = 0,
-    VDEV_ACCESS_HWORD = 1,
-    VDEV_ACCESS_WORD = 2,
-    VDEV_ACCESS_RESERVED = 3
+    VDEV_ACCESS_HWORD,
+    VDEV_ACCESS_WORD,
+    VDEV_ACCESS_RESERVED
 };
 
-/*
- * @write   0: read, 1: write
- * @offset  offset in bytes from base address
- * @pvalue  address to input value (if write) or output value (if read)
- * return: HVMM_STATUS_SUCCESS if successful, failed otherwise
- */
+enum vdev_level {
+    VDEV_LEVEL_LOW = 0,
+    VDEV_LEVEL_MIDDLE,
+    VDEV_LEVEL_HIGH,
+    VDEV_LEVEL_MAX
+};
+
+#define VDEV_ERROR -1
+#define VDEV_NOT_FOUND -1
+
+struct arch_vdev_trigger_info {
+    /** Exception Class */
+    uint32_t ec;
+    /** Instruction Specific Syndrome */
+    uint32_t iss;
+    /** Fault Address */
+    uint32_t fipa;
+    /** Instruction Length */
+    uint32_t il;
+    enum vdev_access_size sas;
+    uint32_t *value;
+};
+
 typedef hvmm_status_t (*vdev_callback_t)(uint32_t wnr, uint32_t offset,
         uint32_t *pvalue, enum vdev_access_size access_size);
 
-struct vdev_info {
-    char *name;
+typedef void(*vdev_irq_callback_t)(void *pdata);
+
+struct vdev_memory_map {
     unsigned int base;
     unsigned int size;
-    vdev_callback_t handler;
 };
 
-void vdev_init(void);
-int register_vdev(void);
-hvmm_status_t vdev_reg_device(struct vdev_info *new_vdev);
-hvmm_status_t vdev_emulate(uint32_t fipa, uint32_t wnr,
-                enum vdev_access_size access_size, uint32_t srt,
-                struct arch_regs *regs);
+typedef int (*initcall_t)(void);
+
+extern initcall_t __vdev_module_high_start[];
+extern initcall_t __vdev_module_high_end[];
+extern initcall_t __vdev_module_middle_end[];
+extern initcall_t __vdev_module_low_end[];
+
+#define __define_vdev_module(level, fn, id) \
+    static initcall_t __initcall_##fn##id __attribute__((__used__)) \
+    __attribute__((__section__(".vdev_module" level ".init"))) = fn
+
+#define vdev_module_high_init(fn)       __define_vdev_module("0", fn, 1)
+#define vdev_module_middle_init(fn)     __define_vdev_module("1", fn, 2)
+#define vdev_module_low_init(fn)        __define_vdev_module("2", fn, 3)
+
+struct vdev_ops {
+    /** Initalize virtual registers */
+    hvmm_status_t (*init)(void);
+
+    /**
+     *  This function is used by the virtual device framework, when the
+     *  trap is occurred, finding the virtual device using the this function.
+     */
+    int32_t (*check)(struct arch_vdev_trigger_info *, struct arch_regs *);
+
+    /** Read a virtual register */
+    int32_t (*read)(struct arch_vdev_trigger_info *, struct arch_regs *);
+
+    /** Write a virtual register */
+    int32_t (*write)(struct arch_vdev_trigger_info *, struct arch_regs *);
+
+    /** Currently unused function */
+    hvmm_status_t (*enable)(void);
+
+    /** Currently unused function */
+    hvmm_status_t (*disable)(void);
+
+    /** Currently unused function */
+    hvmm_status_t (*request_irq)(vdev_irq_callback_t, void *user);
+
+    /** Currently unused function */
+    hvmm_status_t (*notify_irq)(uint32_t);
+
+    /** Post for remain on the job */
+    hvmm_status_t (*post)(struct arch_vdev_trigger_info *, struct arch_regs *);
+
+    /** Dump state of the vdev */
+    hvmm_status_t (*dump)(void);
+
+};
+
+struct vdev_module {
+    /** tag must be initialized to HAL_TAG */
+    uint32_t tag;
+
+    /**
+     * Version of the module-specific device API. This value is used by
+     * the derived-module user to manage different device implementations.
+     * The user who uses this module is responsible for checking
+     * the module_api_version and device version fields to ensure that
+     * the user is capable of communicating with the specific module
+     * implementation.
+     *
+     */
+    uint32_t version;
+
+    /** Identifier of module */
+    const char *id;
+
+    /** Name of this module */
+    const char *name;
+
+    /** Author/owner/implementor of the module */
+    const char *author;
+
+    /** Virtual Device Operation */
+    struct vdev_ops *ops;
+
+};
+
+hvmm_status_t vdev_register(int level, struct vdev_module *module);
+int32_t vdev_find(int level, struct arch_vdev_trigger_info *info,
+        struct arch_regs *regs);
+int32_t vdev_read(int level, int num, struct arch_vdev_trigger_info *info,
+            struct arch_regs *regs);
+int32_t vdev_write(int level, int num, struct arch_vdev_trigger_info *info,
+            struct arch_regs *regs);
+hvmm_status_t vdev_post(int level, int num, struct arch_vdev_trigger_info *info,
+            struct arch_regs *regs);
+hvmm_status_t vdev_init(void);
 
 #endif /* __VDEV_H_ */
--- hypervisor/trap.h
+++ hypervisor/trap.h
@@ -31,6 +31,67 @@
 #define EXTRACT_IL      25
 #define HSR_ISS_BIT     0x01FFFFFF
 
+/*
+ * ISS encoding for Data Abort exceptions taken to Hyp mode as beloww
+ * ISS[24] : instruction syndrome valid. 0 is invalid information in ISS.
+ * 1 is valid information in ISS
+ * when ISS[24] is 0, we don't need to extract information
+ * from the rest of ISS field
+ * when ISS[24] is 1, we need to extract information from ISS[26:13]
+ *
+ * ISS[26:13] is consist of 10 parts. Details as below
+ *
+ * - ISS[23:22] is an access size.
+ * - e.g. byte, hardword, word
+ * - ISS[21] is a sign extend
+ * - e.g. 1 is not sign, 1 is sign
+ * - ISS[20] is reserved
+ * - ISS[19:16] is for register transfer ?
+ * - ISS[15:10] is reserved
+ * - ISS[9] is an external abort type. It is IMPLEMENTATION_DEFINED
+ * - ISS[8] is a cache maintenance. For synchronous fault, it should
+ * need a cache maintenance.
+ * - ISS[7] is a stage 2 fault for a stage 1 translation table walk
+ * - ISS[6] is synchronous abort that was caused by a write or read operation
+ * - ISS[5:0] is a data fault status code(DFSC)
+ * Additional register we should reference a DFSR
+ */
+
+#define ISS_VALID                           0x01000000
+
+#define ISS_FSR_MASK                        0x0000003F
+#define ISS_TRANS_FAULT_MASK                0x07
+#define TRANS_FAULT_LEVEL1                  0x05
+#define TRANS_FAULT_LEVEL2                  0x06
+#define TRANS_FAULT_LEVEL3                  0x07
+#define ACCESS_FAULT_LEVEL0                 0x08
+#define ACCESS_FAULT_LEVEL1                 0x09
+#define ACCESS_FAULT_LEVEL2                 0x0A
+#define ACCESS_FAULT_LEVEL3                 0x0B
+
+#define ISS_WNR_SHIFT                       6
+#define ISS_WNR                             (1 << ISS_WNR_SHIFT)
+
+#define ISS_SAS_SHIFT                       22
+#define ISS_SAS_MASK                        (0x3 << ISS_SAS_SHIFT)
+#define ISS_SAS_BYTE                        0x0
+#define ISS_SAS_HWORD                       0x1
+#define ISS_SAS_WORD                        0x2
+#define ISS_SAS_RESERVED                    0x3
+
+#define ISS_SSE_SHIFT                       21
+#define ISS_SSE_MASK                        (0x1 << ISS_SSE_SHIFT)
+
+#define ISS_SRT_SHIFT                       16
+#define ISS_SRT_MASK                        (0xf << ISS_SRT_SHIFT)
+
+/* HPFAR */
+#define HPFAR_INITVAL                       0x00000000
+#define HPFAR_FIPA_MASK                     0xFFFFFFF0
+#define HPFAR_FIPA_SHIFT                    4
+#define HPFAR_FIPA_PAGE_MASK                0x00000FFF
+#define HPFAR_FIPA_PAGE_SHIFT               12
+
 struct arch_regs *trap_saved_regs(void);
 hvmm_status_t trap_hvc_dabort(unsigned int iss, struct arch_regs *regs);
 enum hyp_hvc_result _hyp_hvc_service(struct arch_regs *regs);
