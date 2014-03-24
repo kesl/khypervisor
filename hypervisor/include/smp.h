95715c6928168b5d3a7b4fe658d473875afd0467
xiane <sangch.go@gmail.com>
2014-03-24 22:20:26 +0900

    Added comments for symmetric multiprocessing.

--- hypervisor/include/smp.h
+++ hypervisor/include/smp.h
@@ -5,14 +5,21 @@
 #include "armv7_p15.h"
 
 #define MPIDR_MASK 0xFFFFFF
+#define MPIDR_CPUID_MASK 0xFF
 
+/**
+ * @brief Gets current CPU ID of the Symmetric MultiProcessing(SMP).
+ *
+ * Read the value from Multiprocessor ID Register(MPIDR) and obtains the CPU ID
+ * by masking.
+ * - Coretex-A15
+ *   - MPIDR[1:0] - CPUID - 0, 1, 2, OR 3
+ *   - MPIDR[7:2] - Reserved, Read as zero
+ * @return The current CPU ID.
+ */
 static inline uint32_t smp_processor_id(void)
 {
-    /* Cortex-A15,
-     * MPIDR[1:0] - CPUID - 0,1, 2, or 3
-     * MPIDR[7:2] - Reserved, Read as zero
-     */
-    return read_mpidr() & MPIDR_MASK;
+    return read_mpidr() & MPIDR_MASK & MPIDR_CPUID_MASK;
 }
 
 #endif
