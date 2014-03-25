334bd9b04021067c6512c7d3a53805220cca943c
Wonseok Ko <magicyaba@gmail.com>
2014-03-25 16:57:42 +0900

    Added trap handler for mcr/mrc access to CP15

---
+++ hypervisor/include/traps.h
@@ -0,0 +1,9 @@
+#ifndef _TRAPS_H_
+#define _TRAPS_H_
+
+void emulate_access_to_cp15(unsigned int iss, unsigned int il);
+void emulate_access_to_cp14(unsigned int iss, unsigned int il);
+void emulate_access_to_cp10(unsigned int iss, unsigned int il);
+void emulate_wfi_wfe(unsigned int iss, unsigned int il);
+
+#endif
