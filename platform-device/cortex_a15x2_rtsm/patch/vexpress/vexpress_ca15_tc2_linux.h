affa3a51fc837400bf405f5a630c3f72b19ba0c8
xiane <sangch.go@gmail.com>
2014-05-26 20:24:13 +0900

    Modify board configuration files and add memory linker scripts and u-boot command files to support both rtsm and vexpress

---
+++ platform-device/cortex_a15x2_rtsm/patch/vexpress/vexpress_ca15_tc2_linux.h
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 2013 Linaro
+ * Andre Przywara, <andre.przywara@linaro.org>
+ *
+ * Configuration for Versatile Express. Parts were derived from other ARM
+ *   configurations.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __VEXPRESS_CA15X2_TC2_h
+#define __VEXPRESS_CA15X2_TC2_h
+
+#define CONFIG_VEXPRESS_EXTENDED_MEMORY_MAP
+#include "vexpress_common.h"
+#define CONFIG_BOOTP_VCI_STRING     "U-boot.armv7.vexpress_ca15x2_tc2"
+
+#define CONFIG_SYSFLAGS_ADDR	0x1c010030
+#define CONFIG_SMP_PEN_ADDR	CONFIG_SYSFLAGS_ADDR
+
+#define CONFIG_ARMV7_VIRT
+
+#define CONFIG_BOOTCOMMAND "cp 0xc000000 0xf0000000 0xbf7c;cp 0xc100000 0xa0000000 0x6285FC;cp 0x3000000 0xd0000000 0x101a44;go 0xf000004c"
+
+#endif
