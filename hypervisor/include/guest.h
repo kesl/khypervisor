15759648510d8b36da3c991cc41b2d62be884aef
joohyun kyong <joohyun0115@gmail.com>
2014-06-14 07:39:35 +0900

    smp : remove hardcoded value in memory

--- hypervisor/include/guest.h
+++ hypervisor/include/guest.h
@@ -72,12 +72,12 @@ struct guest_module {
 
 };
 
-extern uint32_t guest_bin_start;
-extern uint32_t guest_bin_end;
-extern uint32_t guest2_bin_start;
+extern uint32_t _guest0_bin_start;
+extern uint32_t _guest0_bin_end;
+extern uint32_t _guest1_bin_start;
 #ifdef _SMP_
-extern uint32_t guest_secondary_bin_start;
-extern uint32_t guest_secondary_bin_end;
+extern uint32_t _guest2_bin_start;
+extern uint32_t _guest2_bin_end;
 #endif
 extern struct guest_module _guest_module;
 extern struct guest_struct *_current_guest[NUM_CPUS];
