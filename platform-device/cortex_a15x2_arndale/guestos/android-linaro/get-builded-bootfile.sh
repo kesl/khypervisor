6c961cb2a9a3969614c0e134685ba5c2b8a35015
emarnsndiej <emarnsndiej@naver.com>
2014-08-29 13:49:40 +0900

    add get builded bootfile script

---
+++ platform-device/cortex_a15x2_arndale/guestos/android-linaro/get-builded-bootfile.sh
@@ -0,0 +1,3 @@
+wget http://keslproject.iptime.org/mediawiki/images/4/4d/Arndale-bl1.bin
+wget http://keslproject.iptime.org/mediawiki/images/a/ae/Smdk5250-spl.bin
+wget http://keslproject.iptime.org/mediawiki/images/7/71/U-boot.bin
