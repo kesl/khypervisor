da2021cc0696293eb5fb4b1007e2e03ce8d4a818
in1004kyu <in1004kyu@gmail.com>
2014-09-30 12:56:14 +0900

    [Guestloader] : Code refactoring. Guest start address used symbolic constants.

--- platform-device/cortex_a15x2_rtsm/guestos/guestloader/guestloader.h
+++ platform-device/cortex_a15x2_rtsm/guestos/guestloader/guestloader.h
@@ -1,7 +1,9 @@
 #ifndef __GUESTLOADER_H__
 #define __GUESTLOADER_H__
-
+#include "memmap.cfg"
 #define MACHINE_TYPE      2272
+#define GUEST_START_ADDR CFG_LOADER_PHYS_START
+#define ZIMAGE_PHYS_ADDR CFG_GUEST_START
 
 /** @brief Flag for autoboot
  *  @param flag If flag is '0', it is auto boot, otherwise CLI boot
