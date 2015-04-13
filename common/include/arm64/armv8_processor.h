6cefd24417a19a0de7a431d6fb755dd1fd01d652
xiane <sangch.go@gmail.com>
2015-04-13 19:31:33 +0900

    [mustang] remove stringify macro

--- common/include/arm64/armv8_processor.h
+++ common/include/arm64/armv8_processor.h
@@ -1,7 +1,6 @@
 #ifndef __ARMV8_PROCESSOR_H__
 #define __ARMV8_PROCESSOR_H__
 #include "arch_types.h"
-#include "stringify.h"
 
 #define read_sr64(name)       ({ uint64_t rval; asm volatile (\
                               " mrs %0, "#name \
@@ -12,7 +11,7 @@
                                 : : "r" ((val)) : "memory", "cc")
 
 #define read_sr32(name)       ({ unint32_t rval; asm volatile (\
-                              " mrs %0, "__stringify(name) \
+                              " mrs %0, "#name \
                                 : "=r" (rval) " " "memory", "cc"); rval; })
 
 #define write_sr32(val, name) asm volatile(\
