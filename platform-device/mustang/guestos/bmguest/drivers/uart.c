8fe4ea0b790737a21c04f639295ce9934315a64a
xiane <sangch.go@gmail.com>
2015-06-08 00:20:26 +0900

    [mustang] Refactoring uart configuration.

--- platform-device/mustang/guestos/bmguest/drivers/uart.c
+++ platform-device/mustang/guestos/bmguest/drivers/uart.c
@@ -1,8 +1,14 @@
 /* UART Base Address determined by Hypervisor's Stage 2 Translation Table */
 
-#define UART_BASE       ((volatile unsigned int *) 0x1C020000)
+#define UART_BASE       (0x1C020000)
 
 static char _dummy_byte;
+
+volatile unsigned char *pUART;
+#define UART_LCR 0x03
+#define UART_FCR 0x02
+#define UART_IER 0x01
+#define UART_LSR 0x05
 
 void uart_init(void)
 {
@@ -11,30 +17,38 @@ void uart_init(void)
        Currently, intialized by Hypervisor for FastModels
        RTSM_VE, as a workaround
      */
-    /* ibrd 0x24 */
-    /* UART_BASE[9] = 0x10; */
-    /* UART_BASE[12] = 0xc300; */
+    pUART = (unsigned char *) UART_BASE;
+
+    // no ahbc setting, no clock setting
+//    pUART[UART_LCR] = 0x03;
+//    pUART[UART_FCR] = 0x00;
+//    pUART[UART_IER] = 0x00;
 }
 
 void uart_print(char *str)
 {
-    char *pUART = (char *) UART_BASE;
     while (*str)
-        *pUART = *str++;
+        uart_putc(*str++);
 }
 
 void uart_putc(char c)
 {
-    volatile char *pUART = (char *) UART_BASE;
+    int i;
+    if (c == '\n')
+        uart_putc('\r');
+    for( i=1; i<3500; i++)
+    {
+        if ( (pUART[UART_LSR] & 0x20) == 0x20)
+            break;
+        // have to? add deploy function
+    }
     *pUART = c;
 }
-
-void uart_print_hex32(unsigned int v)
+void uart_print_hex(unsigned int v)
 {
     unsigned int mask8 = 0xF;
     unsigned int c;
     int i;
-    uart_print("0x");
     for (i = 7; i >= 0; i--) {
         c = ((v >> (i * 4)) & mask8);
         if (c < 10)
@@ -43,4 +57,18 @@ void uart_print_hex32(unsigned int v)
             c += 'A' - 10;
         uart_putc((char) c);
     }
+}
+
+void uart_print_hex32(unsigned int v)
+{
+    uart_print("0x");
+    uart_print_hex(v);
+}
+
+void uart_print_hex64(unsigned int v)
+{
+    uart_print("0x");
+    uart_print_hex(v >> 32);
+    uart_print("_");
+    uart_print_hex((unsigned int)(v & 0xFFFFFFFF));
 }
