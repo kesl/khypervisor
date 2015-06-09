b6f64111c99fd2a05ac860c60cde1b8aba99f8e3
xiane <sangch.go@gmail.com>
2015-06-09 17:38:23 +0900

    [musatng] Added reboot function and applied that to sync and unhandled handlers.

--- common/guest/core/exception.c
+++ common/guest/core/exception.c
@@ -2,17 +2,23 @@
 #define ARCH_REGS_NUM_GPR   31
 struct arch_regs {
     unsigned int cpsr;
-    unsigned int pc;
-    unsigned int gpr[ARCH_REGS_NUM_GPR];
-} __attribute((packed));
+    unsigned long pc;
+    unsigned long gpr[ARCH_REGS_NUM_GPR];
+};
 
+static void _reboot()
+{ unsigned int *reboot = (unsigned int *) 0x17000014;
+    *reboot = 0x1;
+}
 
 void _except_unhandled(struct arch_regs *regs)
 {
+    _reboot();
 }
 
 void _except_svc(struct arch_regs *regs)
 {
+    _reboot();
 }
 
 void _except_irq(struct arch_regs *regs)
