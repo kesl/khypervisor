3f620e796bf6860d51fe3fbb69cd520bfd175b7a
xiane <sangch.go@gmail.com>
2015-06-16 00:32:06 +0900

    [mustang] Added fdt(dtb), linux Image physical address definition.

--- platform-device/mustang/guestos/guestloader/guestloader.h
+++ platform-device/mustang/guestos/guestloader/guestloader.h
@@ -1,9 +1,9 @@
 #ifndef __GUESTLOADER_H__
 #define __GUESTLOADER_H__
 #include "memmap.cfg"
-#define MACHINE_TYPE        2272
 #define GUEST_START_ADDR    LDS_LOADER_PHYS_START
-#define ZIMAGE_PHYS_ADDR    LDS_GUEST_START
+#define ZIMAGE_PHYS_ADDR    LDS_ZIMAGE_START
+#define FDT_PHYS_ADDR       LDS_FDT_START
 /** @brief Flag for autoboot
  *  @param flag If flag is '0', it is auto boot, otherwise CLI boot
  */
