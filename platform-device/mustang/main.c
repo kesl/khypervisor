b8b11f3c296859097e64625f3a2d204fa0ffb35f
xiane <sangch.go@gmail.com>
2015-07-01 12:14:18 +0900

    [mustang] Removed unused comment.

--- platform-device/mustang/main.c
+++ platform-device/mustang/main.c
@@ -203,55 +203,8 @@ void setup_interrupt()
         }
     }
 
-    /*
-     *  vimm-0, pirq-69, virq-69 = pwm timer driver
-     *  vimm-0, pirq-32, virq-32 = WDT: shared driver
-     *  vimm-0, pirq-34, virq-34 = SP804: shared driver
-     *  vimm-0, pirq-35, virq-35 = SP804: shared driver
-     *  vimm-0, pirq-36, virq-36 = RTC: shared driver
-     *  vimm-0, pirq-38, virq-37 = UART: dedicated driver IRQ 37 for guest 0
-     *  vimm-1, pirq-39, virq-37 = UART: dedicated driver IRQ 37 for guest 1
-     *  vimm-2, pirq,40, virq-37 = UART: dedicated driver IRQ 37 for guest 2
-     *  vimm-3, pirq,48, virq-37 = UART: dedicated driver IRQ 38 for guest 3 -ch
-     *  vimm-0, pirq-43, virq-43 = ACCI: shared driver
-     *  vimm-0, pirq-44, virq-44 = KMI: shared driver
-     *  vimm-0, pirq-45, virq-45 = KMI: shared driver
-     *  vimm-0, pirq-47, virq-47 = SMSC 91C111, Ethernet - etc0
-     *  vimm-0, pirq-41, virq-41 = MCI - pl180
-     *  vimm-0, pirq-42, virq-42 = MCI - pl180
-     */
-//    for(i=0; i< 24; i++)
-//        DECLARE_VIRQMAP(_guest_virqmap, 0, i, i);
-//    for(i=26; i<29 ; i++)
-//        DECLARE_VIRQMAP(_guest_virqmap, 0, i, i);
     for(i=32; i< MAX_IRQS; i++)
         DECLARE_VIRQMAP(_guest_virqmap, 0, i, i);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 6, 6);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 3, 3);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 1, 1);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 16, 16);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 17, 17);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 18, 18);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 19, 19);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 31, 31);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 32, 32);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 33, 33);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 34, 34);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 35, 35);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 36, 36);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 37, 38);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 38, 37);
-//    DECLARE_VIRQMAP(_guest_virqmap, 1, 39, 37);
-//    DECLARE_VIRQMAP(_guest_virqmap, 2, 40, 37);
-//    DECLARE_VIRQMAP(_guest_virqmap, 3, 48, 37);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 41, 41);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 42, 42);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 43, 43);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 44, 44);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 45, 45);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 46, 46);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 47, 47);
-//    DECLARE_VIRQMAP(_guest_virqmap, 0, 69, 69);
 }
 
 void setup_memory()
