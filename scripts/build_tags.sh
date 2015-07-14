1f744c96ec59f0fa6126e621b8d104df65b2726a
xiane <sangch.go@gmail.com>
2015-07-14 18:16:34 +0900

    [Tag script] Added tag and cscope building options to exclude linux, ucos-ii and u-boot directories.

--- scripts/build_tags.sh
+++ scripts/build_tags.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-find . -name "*.[chCHS]" -exec etags -a {} \;
-cscope -b -R
+find . ! \( \( -type d -path './*/guestos/linux' -o -path './*/u-boot*' -o -path './*/ucos-ii' \) -prune \) -name "*.[chCHS]" -exec etags -a {} \;
+find . ! \( \( -type d -path './*/guestos/linux' -o -path './*/u-boot*' -o -path './*/guestos/ucos-ii' \) -prune \) -name "*.[chCHS]" -print >> ./cscope.files
+cscope -b -q -k
+rm cscope.files
